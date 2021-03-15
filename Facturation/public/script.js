d0d901cf6f2dd5edc68533e133081d8d252e0d71
hachemchrayti <hachemfst@gmail.Com>
2021-03-15 16:03:39 +0100

    Retrieve data from form using class and validate retrieved data

--- Facturation/public/script.js
+++ Facturation/public/script.js
@@ -1,2 +1,52 @@
 "use strict";
-console.log("hachem");
+class FormInput {
+    constructor() {
+        this.form = document.getElementById("form");
+        this.type = document.getElementById("type");
+        this.firstName = document.getElementById("firstName");
+        this.lastName = document.getElementById("lastName");
+        this.address = document.getElementById("address");
+        this.country = document.getElementById("country");
+        this.town = document.getElementById("town");
+        this.zip = document.getElementById("zip");
+        this.product = document.getElementById("product");
+        this.quantity = document.getElementById("quantity");
+        this.price = document.getElementById("price");
+        this.tva = document.getElementById("tva");
+        //call Listener
+        this.submitFormListener();
+    }
+    //Listeners
+    submitFormListener() {
+        this.form.addEventListener('submit', this.handleFormSubmit.bind(this));
+    }
+    handleFormSubmit(e) {
+        e.preventDefault();
+        const inputs = this.inputDatas();
+        if (Array.isArray(inputs)) {
+            const [type, firstName, lastName, address, country, town, zip, product, price, quantity, tva] = inputs;
+            console.log(type, firstName, lastName, address, country, town, zip, product, price, quantity, tva);
+        }
+    }
+    inputDatas() {
+        const type = this.type.value;
+        const firstName = this.firstName.value;
+        const lastName = this.lastName.value;
+        const address = this.address.value;
+        const country = this.country.value;
+        const town = this.town.value;
+        const zip = this.zip.valueAsNumber;
+        const product = this.product.value;
+        const quantity = this.quantity.valueAsNumber;
+        const price = this.price.valueAsNumber;
+        const tva = this.tva.valueAsNumber;
+        if (zip > 0 && price > 0 && quantity > 0 && tva > 0) {
+            return [type, firstName, lastName, address, country, town, zip, product, price, quantity, tva];
+        }
+        else {
+            alert("Les valeurs numériques doivent etre > 0 !");
+            return;
+        }
+    }
+}
+new FormInput();
